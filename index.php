bbc9180953067468d83a3960cedab9f4b84fa03d
xiaodong chen <cxd032404@hotmail.com>
2021-04-26 22:37:19 +0800

    mereg

--- index.php
+++ index.php
@@ -3,15 +3,15 @@ $url = $_SERVER['SERVER_NAME'];
 $toJump = "";
 if(strpos($url,"kylinesport.com")>=0)
 {
-    $toJump = "http://efs-openweb.kiringames.cn/downloadPage?code=12JYNO&channelsType=1";
+    $toJump = "hhttp://efs-openweb.kiringames.cn/downloadPage/9BTGWH";
 }
 elseif(strpos($url,"www.kylinesport.com")>=0)
 {
-    $toJump = "http://efs-openweb.kiringames.cn/downloadPage?code=12JYNO&channelsType=1";
+    $toJump = "http://efs-openweb.kiringames.cn/downloadPage/9BTGWH";
 }
 elseif(strpos($url,"m.kylinesport.com")>=0)
 {
-    $toJump = "http://efs-openweb.kiringames.cn/downloadPage?code=12JYNO&channelsType=1";
+    $toJump = "http://efs-openweb.kiringames.cn/downloadPage/9BTGWH";
 }
 if($toJump!="")
 {
