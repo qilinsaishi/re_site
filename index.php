7f89ec284f19f22fa3aa01ae93d2e4b2d432e0a5
xiaodong chen <cxd032404@hotmail.com>
2021-04-27 21:42:51 +0800

    merge

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $url = $_SERVER['SERVER_NAME'];
 $toJump = "";
 if(strpos($url,"kylinesport.com")>=0)
 {
-    $toJump = "hhttp://efs-openweb.kiringames.cn/downloadPage/9BTGWH";
+    $toJump = "http://efs-openweb.kiringames.cn/downloadPage/9BTGWH";
 }
 elseif(strpos($url,"www.kylinesport.com")>=0)
 {
